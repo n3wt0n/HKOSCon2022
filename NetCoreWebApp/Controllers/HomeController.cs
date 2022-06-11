d5e801f0fdc908f54bea2fdde227522ac3dde7d1
Davide Benvegnù aka CoderDave <davide.benvegnu@outlook.com>
2022-06-11 12:07:08 +0800

    Update HomeController.cs

--- NetCoreWebApp/Controllers/HomeController.cs
+++ NetCoreWebApp/Controllers/HomeController.cs
@@ -1,11 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using NetCoreWebApp.Models;
+using System.Threading.Tasks;
+using System.Xml;
 
 namespace NetCoreWebApp.Controllers
 {
@@ -19,6 +23,11 @@ namespace NetCoreWebApp.Controllers
         }
 
         public IActionResult Index()
+        {           
+            return View();
+        }
+        
+        private void DoSomething(HttpContext ctx, string employeeName)
         {
             //XML Injection vulnerability
              using (XmlWriter writer = XmlWriter.Create("employees.xml"))
@@ -39,8 +48,6 @@ namespace NetCoreWebApp.Controllers
             {
                 ctx.Response.Redirect("login");
             }
-            
-            return View();
         }
 
         public IActionResult Privacy()
